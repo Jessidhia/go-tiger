c2d667f7de9274a2b6248a08f03d8455570a0483
Diogo Franco (Kovensky) <diogomfranco@gmail.com>
2013-08-12 23:14:42 -0300

    Add 'unsafe' version of tigerRound
    Marginally faster than the 'mathy' version.

--- round.go
+++ round.go
@@ -1,21 +1,5 @@
 package tiger
 
-func tigerRound(a, b, c, x, mul uint64) (new_a, new_b, new_c uint64) {
-	c ^= x
-	cb := [8]byte{
-		byte((c >> (0 * 8)) & 0xff), byte((c >> (1 * 8)) & 0xff),
-		byte((c >> (2 * 8)) & 0xff), byte((c >> (3 * 8)) & 0xff),
-		byte((c >> (4 * 8)) & 0xff), byte((c >> (5 * 8)) & 0xff),
-		byte((c >> (6 * 8)) & 0xff), byte((c >> (7 * 8)) & 0xff),
-	}
-
-	a -= sBox[0][cb[0]] ^ sBox[1][cb[2]] ^ sBox[2][cb[4]] ^ sBox[3][cb[6]]
-	b += sBox[3][cb[1]] ^ sBox[2][cb[3]] ^ sBox[1][cb[5]] ^ sBox[0][cb[7]]
-	b *= mul
-
-	return a, b, c
-}
-
 func tigerPass(a, b, c uint64, x [8]uint64, mul uint64) (new_a, new_b, new_c uint64) {
 	a, b, c = tigerRound(a, b, c, x[0], mul)
 	b, c, a = tigerRound(b, c, a, x[1], mul)
